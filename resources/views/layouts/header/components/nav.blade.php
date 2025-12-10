37fea6e1dc3ad6c5e77d92362b8e0ac5a2d52c95
InventedPrime <108306912+InventedPrime@users.noreply.github.com>
2025-12-10 18:01:01 -0500

    feature: renamed the search to explore

--- resources/views/layouts/header/components/nav.blade.php
+++ resources/views/layouts/header/components/nav.blade.php
@@ -1,6 +1,6 @@
         <!-- Navigation Links (Desktop) -->
         <nav class="navbar-nav">
             <a href="{{ route('home') }}">Home</a>
-            <a href="{{ route('search') }}">Search</a>
+            <a href="{{ route('search') }}">Explore</a>
             <a href="{{ route('upload') }}">Upload</a>
         </nav>
